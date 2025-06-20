8a52df1288653aa2719e71eeb36b02c286c18771
Marios Elezoglou <71016858+Katapeltis@users.noreply.github.com>
2025-06-20 18:19:08 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,43 +1,50 @@
 import torch
 from sklearn.metrics import f1_score, classification_report
 from datasets import train_test_split_graph
-from gnn_architectures import HeteroGNN, HeteroGAT
-
+from gnn_architectures import HeteroGNN, HeteroGAT, HeteroGraphSAGE
+import torch.nn.functional as F
+import torch.nn as nn
 
 
 def get_model(graph, gnn_type: str):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     num_classes = 2
+    in_feats = graph.ndata['feature'].shape[1]  # Input feature dimension
     etypes = graph.etypes
 
     if gnn_type == 'GCN':
-        in_feats = graph.ndata['feature'].shape[1]
         h_feats = 16  # Hidden layer dimension
         model = HeteroGNN(in_feats, h_feats, num_classes, etypes)
 
     if gnn_type == 'GAT':
-        in_feats = graph.ndata['feature'].shape[1]  # Input feature dimension
         h_feats = 8  # Hidden layer dimension
         num_heads = 4  # Number of attention heads
         model = HeteroGAT(in_feats, h_feats, num_classes, etypes, num_heads)
 
-    return model.to('cpu')
+    if gnn_type == 'GraphSAGE':
+        h_feats = 16
+        aggregator_type = 'mean'
+        model = HeteroGraphSAGE(in_feats, h_feats, num_classes, etypes, aggregator_type)
+
+    return model.to(device)
 
 
-def train(model, graph, optimizer, criterion, train_ratio=0.7, test_ratio=0.15, val_ratio=0.15):
+
+def train(model, mask, graph, optimizer, criterion):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model.train()
     optimizer.zero_grad()
 
     inputs = {'review': graph.ndata['feature']}
-    train_data, _,_ = train_test_split_graph(graph, train_ratio=train_ratio, test_ratio=test_ratio, val_ratio=val_ratio)
 
     graph = graph.to(device)
     graph.ndata['label'] = graph.ndata['label'].to(device)
     
-    logits = model(graph, inputs)
-    logits = logits['review'][train_data.to(device)]
-    labels = graph.ndata['label'][train_data.to(device)]
+    logits,_ = model(graph, inputs)
+    logits = logits['review'][mask]
+    labels = graph.ndata['label'][mask]
     
     loss = criterion(logits, labels)
     loss.backward()
@@ -51,7 +58,7 @@ def evaluate(mask, model, graph):
     with torch.no_grad():
         graph.ndata['feature'] = graph.ndata['feature'].to(device)
         inputs = {'review': graph.ndata['feature']}
-        logits = model(graph, inputs)
+        logits,probs = model(graph, inputs)
         logits = logits['review'][mask]
         labels = graph.ndata['label'][mask]
         
@@ -60,6 +67,6 @@ def evaluate(mask, model, graph):
         accuracy = (preds == labels).float().mean().item()
         
         # Compute F1 score and classification report
-        f1 = f1_score(labels.cpu().numpy(), preds.cpu().numpy(), average='macro')  # macro - micro - binary
+        f1 = f1_score(labels.cpu().numpy(), preds.cpu().numpy(), average='macro')
         report = classification_report(labels.cpu().numpy(), preds.cpu().numpy(), target_names=['Class 0', 'Class 1'])
-        return accuracy, f1, report
+        return accuracy, f1, report, probs
